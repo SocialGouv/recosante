d6d3ec2383295766257f5289f2b37d6cb10f803e
Adrien Chauve <adrien@chauve.eu>
2023-03-20 17:09:51 +0100

    Use uwsgi instead of flask server and fix indice healthz

--- api/startup.sh
+++ api/startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 flask db upgrade
-flask run --host=0.0.0.0
+exec uwsgi
--- indice_pollution/startup.sh
+++ indice_pollution/startup.sh
@@ -2,3 +2,4 @@
 alembic upgrade head
 celery -A indice_pollution.celery_worker.celery purge -Q save_indices --force
 celery -A indice_pollution.celery_worker.celery worker --loglevel=INFO
+exec wsgi
