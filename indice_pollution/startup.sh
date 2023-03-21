55a05f299e96b3dad1c5f77cb42649da38ad587b
Adrien Chauve <adrien@chauve.eu>
2023-03-21 19:08:02 +0100

    fix uwsgi socket

--- indice_pollution/startup.sh
+++ indice_pollution/startup.sh
@@ -2,4 +2,4 @@
 alembic upgrade head
 celery -A indice_pollution.celery_worker.celery purge -Q save_indices --force
 celery -A indice_pollution.celery_worker.celery worker --loglevel=INFO
-exec uwsgi --socket 0.0.0.0:8080 --wsgi wsgi:application --uid app --plugins python3 --protocol uwsgi --enable-threads 
+exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:application --uid app --plugins python3 --protocol uwsgi --enable-threads 
