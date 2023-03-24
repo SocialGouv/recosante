f7e220289e0dd9287535625cd006738987290cf0
Adrien Chauve <adrien@chauve.eu>
2023-03-24 18:19:20 +0100

    fix indice start

--- indice_pollution/start_all.sh
+++ indice_pollution/start_all.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
+alembic upgrade head
+celery -A indice_pollution.celery_worker.celery purge -Q save_indices --force
 exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:application --uid app --plugins python3 --protocol uwsgi --enable-threads --attach-daemon './start_celery.sh' 
--- indice_pollution/start_celery.sh
+++ indice_pollution/start_celery.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-alembic upgrade head
-celery -A indice_pollution.celery_worker.celery purge -Q save_indices --force
 celery -A indice_pollution.celery_worker.celery worker --loglevel=INFO
