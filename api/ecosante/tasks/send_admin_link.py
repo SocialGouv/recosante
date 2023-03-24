5eb1aa7629715f6ab4b6193b3b79e1e5f485834c
Adrien Chauve <adrien@chauve.eu>
2023-03-24 16:43:15 +0100

    try using fake request context

--- api/ecosante/tasks/send_admin_link.py
+++ api/ecosante/tasks/send_admin_link.py
@@ -1,7 +1,7 @@
 import sib_api_v3_sdk
 from ecosante.extensions import celery, sib, admin_authenticator
 from sib_api_v3_sdk.rest import ApiException
-from flask import url_for
+from flask import url_for, current_app
 from celery.utils.log import get_task_logger
 
 logger = get_task_logger(__name__)
@@ -15,38 +15,42 @@ def send_admin_link(email):
 
     token = admin_authenticator.make_token(email)
     email_api = sib_api_v3_sdk.TransactionalEmailsApi(sib)
-    authentication_link = url_for("pages.authenticate", _external=True, token=token)
-    logger.info(f"Admin link: {authentication_link}")
-    try:
-        email_api.send_transac_email(
-            sib_api_v3_sdk.SendSmtpEmail(
-                subject='Lien connexion recosanté',
-                sender=sib_api_v3_sdk.SendSmtpEmailSender(
-                    name= "Recosanté",
-                    email= "hi@recosante.beta.gouv.fr"
-                ),
-                to=[sib_api_v3_sdk.SendSmtpEmailTo(email=email)],
-                reply_to=sib_api_v3_sdk.SendSmtpEmailReplyTo(
-                    name="Recosanté",
-                    email="hi@recosante.beta.gouv.fr"
-                ),
-                html_content=f"""
-                Bonjour,
-                Voici votre <a href="{ authentication_link }">lien pour aller sur l’administration</a>
-                Bonne journée
-                """,
-                text_content=f"""
-                Bonjour,
-                Voici votre lien pour aller sur l’administration : { authentication_link }
-                Bonne journée
-                """
+
+    # Use fake request context as otherwise SERVER_NAME is required to be set for a call to url_for
+    # and using it results in 404 all over the place.
+    with current_app.test_request_context('/', base_url=current_app.config['ROOT_URL']):
+        authentication_link = url_for("pages.authenticate", _external=True, token=token)
+        logger.info(f"Admin link: {authentication_link}")
+        try:
+            email_api.send_transac_email(
+                sib_api_v3_sdk.SendSmtpEmail(
+                    subject='Lien connexion recosanté',
+                    sender=sib_api_v3_sdk.SendSmtpEmailSender(
+                        name= "Recosanté",
+                        email= "hi@recosante.beta.gouv.fr"
+                    ),
+                    to=[sib_api_v3_sdk.SendSmtpEmailTo(email=email)],
+                    reply_to=sib_api_v3_sdk.SendSmtpEmailReplyTo(
+                        name="Recosanté",
+                        email="hi@recosante.beta.gouv.fr"
+                    ),
+                    html_content=f"""
+                    Bonjour,
+                    Voici votre <a href="{ authentication_link }">lien pour aller sur l’administration</a>
+                    Bonne journée
+                    """,
+                    text_content=f"""
+                    Bonjour,
+                    Voici votre lien pour aller sur l’administration : { authentication_link }
+                    Bonne journée
+                    """
+                )
             )
+        except ApiException as e:
+            logger.error(
+                f"Error: {e}"
+            )
+            raise e
+        logger.info(
+            f"Mail d’authentication à l’administration envoyé à {email}"
         )
-    except ApiException as e:
-        logger.error(
-            f"Error: {e}"
-        )
-        raise e
-    logger.info(
-        f"Mail d’authentication à l’administration envoyé à {email}"
-    )
