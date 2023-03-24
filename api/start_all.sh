5b0b1c91506d84616422c527a206c1ec1e1fc7e9
Adrien Chauve <adrien@chauve.eu>
2023-03-24 11:59:24 +0100

    Fix add missing celery for api

---
+++ api/start_all.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+flask db upgrade
+exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:app --enable-threads --master --processes 4 --attach-daemon './start_celery.sh' 
--- api/startup.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-flask db upgrade
-exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:app --enable-threads --master --processes 4
