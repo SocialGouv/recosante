5115fab4b7dfdb0817762e8f10a8816be7f2fb5d
Adrien Chauve <adrien@chauve.eu>
2023-03-30 13:46:54 +0200

    fix: Update uwsgi startup for indice and remove warning

--- indice_pollution/start_all.sh
+++ indice_pollution/start_all.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 alembic upgrade head
 celery -A indice_pollution.celery_worker.celery purge -Q save_indices --force
-exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:application --uid app --plugins python3 --protocol uwsgi --enable-threads --attach-daemon './start_celery.sh' 
+exec uwsgi --http 0.0.0.0:8080 --wsgi wsgi:application --enable-threads --master --processes 1 --attach-daemon './start_celery.sh' 
