ae32ff40f97658f1b9b71f9d51eca1ba75e4d131
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-08-11 09:16:09 +0200

    fix: lint

--- frontend/src/components/subscription/Progress.js
+++ frontend/src/components/subscription/Progress.js
@@ -11,8 +11,8 @@ export default function Progress({ steps, currentStep }) {
               className={[
                 "shrink-0 grow basis-0 text-center",
                 step.title === currentStep.title
-                  ? "text-main font-medium"
-                  : "text-main/30 font-normal",
+                  ? "font-medium text-main"
+                  : "font-normal text-main/30",
               ].join(" ")}
             >
               {step.title}
@@ -23,9 +23,9 @@ export default function Progress({ steps, currentStep }) {
       <p className="mb-2 mt-8 w-full text-center md:hidden">
         {currentStep.title}
       </p>
-      <div className="bg-main/20 flex h-1 w-full overflow-hidden rounded-full md:h-2">
+      <div className="flex h-1 w-full overflow-hidden rounded-full bg-main/20 md:h-2">
         <div
-          className="bg-main h-full w-full origin-left rounded-full border transition-transform"
+          className="h-full w-full origin-left rounded-full border bg-main transition-transform"
           style={{
             transform: `scaleX(${currentStep.step / steps.length})`,
           }}
