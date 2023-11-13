d849995b7b5829187eb6a21b40122785182dda81
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-13 16:40:44 +0100

    fix: new cron hour

--- api/ecosante/newsletter/tasks/__init__.py
+++ api/ecosante/newsletter/tasks/__init__.py
@@ -13,7 +13,7 @@ def setup_periodic_tasks(sender, **kwargs):
     if sender.conf.env != "production":
         return
     sender.add_periodic_task(
-        crontab(minute='15', hour='15', day_of_week='*/1'),
+        crontab(minute='10', hour='16', day_of_week='*/1'),
         # this cron is the final call to send the daily newsletter
         # the newsletter is sent even if some indicators are missing
         import_send_and_report.s(
