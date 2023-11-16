3c00129e24a00354eba9aee0b23c3e9313bc53b8
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-16 10:19:04 +0100

    fix: cron schedule

--- api/ecosante/newsletter/tasks/__init__.py
+++ api/ecosante/newsletter/tasks/__init__.py
@@ -6,16 +6,17 @@ from ecosante.newsletter.tasks.import_in_sb import \
 from ecosante.newsletter.tasks.send_webpush_notifications import \
     send_webpush_notifications  # noqa
 
+print("on_after_configure")
+
 
 @celery.on_after_configure.connect
-print(f"Newsletter task loaded")
 def setup_periodic_tasks(sender, **kwargs):
     _ = kwargs
     if sender.conf.env != "production":
         print(f"Skipping periodic tasks setup for env {sender.conf.env}")
         return
     sender.add_periodic_task(
-        crontab(minute='00', hour='18', day_of_week='*/1'),
+        crontab(minute='40', hour='09', day_of_week='*/1'),
         # this cron is the final call to send the daily newsletter
         # the newsletter is sent even if some indicators are missing
         import_send_and_report.s(
