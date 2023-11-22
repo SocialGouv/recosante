ae822169647ece51fee611d14729c4b6b4e38525
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-11-22 06:39:29 +0100

    feat: PREPROD WORKING - setting final schedule for J-1 newsletter

--- api/ecosante/newsletter/tasks/__init__.py
+++ api/ecosante/newsletter/tasks/__init__.py
@@ -17,7 +17,7 @@ def setup_periodic_tasks(sender, **kwargs):
         return
     print(f"Setting periodic tasks setup for env {sender.conf.env}")
     sender.add_periodic_task(
-        crontab(minute='30', hour='16-18', day_of_week='*/1'),
+        crontab(minute='0', hour='16', day_of_week='*/1'),
         # this cron is the final call to send the daily newsletter
         # the newsletter is sent even if some indicators are missing
         import_send_and_report.s(
@@ -33,7 +33,7 @@ def setup_periodic_tasks(sender, **kwargs):
         routing_key='send_newsletter.import_send_and_report'
     )
     sender.add_periodic_task(
-        crontab(minute='15', hour='09', day_of_week='*/1'),
+        crontab(minute='0', hour='16', day_of_week='*/1'),
         send_webpush_notifications.s(force_send=True),
         queue='send_newsletter',
         routing_key='send_newsletter.send_webpush_notifications'
