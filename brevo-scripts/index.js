441c2b84ee18d3bdcbdf5bc36817320869f18cfc
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-01 11:06:55 +0100

    fix

--- brevo-scripts/index.js
+++ brevo-scripts/index.js
@@ -52,10 +52,12 @@ https://developers.brevo.com/reference/updatecampaignstatus
 // TIP ids are in the form 9967, 9966, etc.
 // So we don'tneed to fetch the campaigns abov
 
-for (let i = 9167; i > 0; i--) {
+for (let i = 7791; i > 0; i--) {
   await new Promise((resolve) => setTimeout(resolve, 1000));
-  await BrevoApi.put({
+  const result = await BrevoApi.put({
     path: `/emailCampaigns/${i}/status`,
     body: { status: "archive" },
   });
+  if (result === 429) break;
+  console.log(i, result);
 }
