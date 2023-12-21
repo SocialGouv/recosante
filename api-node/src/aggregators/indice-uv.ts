73fd150fc8997ca16bcf50461be83e2b6a7d5796
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-21 18:33:50 +0100

    feat: indicators models

---
+++ api-node/src/aggregators/indice-uv.ts
@@ -0,0 +1,12 @@
+import dotenv from "dotenv";
+dotenv.config({ path: "./.env" });
+
+function getIndiceUV() {
+  // return fetch(
+  //   `https://api.openweathermap.org/data/2.5/uvi?lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&appid=${process.env.API_KEY}`
+  // )
+  //   .then((res) => res.json())
+  //   .then((res) => res.value);
+}
+
+getIndiceUV();
