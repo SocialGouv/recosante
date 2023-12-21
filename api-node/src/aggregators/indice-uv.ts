a2792bdc5b1009b3bd777f074e38f8bf2ecc2593
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2023-12-21 19:00:03 +0100

    add cities + example

--- api-node/src/aggregators/indice-uv.ts
+++ api-node/src/aggregators/indice-uv.ts
@@ -2,11 +2,23 @@ import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 
 function getIndiceUV() {
-  // return fetch(
-  //   `https://api.openweathermap.org/data/2.5/uvi?lat=${process.env.LATITUDE}&lon=${process.env.LONGITUDE}&appid=${process.env.API_KEY}`
-  // )
-  //   .then((res) => res.json())
-  //   .then((res) => res.value);
+  /*
+  1. fetch data
+  2. map the v_commune_2023.csv file by insee_code
+  3. for each insee_code, concatenate the indice_uv data
+  v_commune_2023.map(commune => {
+    const iundiceUvRow = indicUvData.find(uv => uv.insee_code === commune.insee_code);
+    return {
+      validity_start: indiceUvRow.validity_start,
+      validity_end: indiceUvRow.validity_end,
+      diffusion_date: indiceUvRow.diffusion_date,
+      municipality_insee_code: commune.insee_code,
+      uv_j0: indiceUvRow.uv_j0,
+      ....
+    }
+  })
+  4. save data to database
+  */
 }
 
 getIndiceUV();
