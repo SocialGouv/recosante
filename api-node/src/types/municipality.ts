8abf1b7ed075b1dd666c0cda9b2a8d909e570702
charlesdoiron <doironcharles@gmail.com>
2024-01-04 14:19:10 +0100

    Update TNCC type in municipality.ts

--- api-node/src/types/municipality.ts
+++ api-node/src/types/municipality.ts
@@ -16,12 +16,12 @@ export interface MunicipalityJSON {
 export type DepartmentCode = string;
 
 type TNCC =
-  | 0 // Pas d'article et le nom commence par une consonne sauf H muet	charnière = DE
-  | 1 // Pas d'article et le nom commence par une voyelle ou un H muet	charnière = D'
-  | 2 // Article = LE	charnière = DU
-  | 3 // Article = LA	charnière = DE LA
-  | 4 // Article = LES	charnière = DES
-  | 5 // Article = L'	charnière = DE L'
-  | 6 // Article = AUX	charnière = DES
-  | 7 // Article = LAS	charnière = DE LAS
-  | 8; // Article = LOS	charnière = DE LOS
+  | '0' // Pas d'article et le nom commence par une consonne sauf H muet	charnière = DE
+  | '1' // Pas d'article et le nom commence par une voyelle ou un H muet	charnière = D'
+  | '2' // Article = LE	charnière = DU
+  | '3' // Article = LA	charnière = DE LA
+  | '4' // Article = LES	charnière = DES
+  | '5' // Article = L'	charnière = DE L'
+  | '6' // Article = AUX	charnière = DES
+  | '7' // Article = LAS	charnière = DE LAS
+  | '8'; // Article = LOS	charnière = DE LOS
