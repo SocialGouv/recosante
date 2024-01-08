7765ee109714b28a9788ecccadb1ad43c2e21e06
Charlesdoiron <doironcharles@gmail.com>
2024-01-08 16:42:47 +0100

    Formating data after multiple fetch in atmo indice, still lot of format pb (#332)

--- api-node/src/aggregators/atmo.ts
+++ api-node/src/aggregators/atmo.ts
@@ -2,6 +2,8 @@ import dayjs from 'dayjs';
 import utc from 'dayjs/plugin/utc';
 import dotenv from 'dotenv';
 import { MunicipalityJSON } from '~/types/municipality';
+import fs from 'fs';
+
 dotenv.config({ path: './.env' });
 dayjs.extend(utc);
 
@@ -12,7 +14,7 @@ function logStep(step: string) {
 }
 const nowUtcFormat = dayjs.utc().format();
 
-// Type \\
+// Types \\
 type Response = {
   attributes: {
     date_ech: number;
@@ -47,7 +49,19 @@ type Response = {
   };
 };
 
+type AtmoSchema = {
+  [key: string]: {
+    no2: number;
+    so2: number;
+    o3: number;
+    pm10: number;
+    pm25: number;
+    max_value: number;
+  };
+};
 import municipalitiesJson from './../../data/municipalities.json';
+import prisma from '~/prisma';
+import { DataAvailabilityEnum } from '@prisma/client';
 
 // URL \\
 const CORSE_URL =
@@ -107,68 +121,230 @@ function getIleDeFranceInseeCodeQueryParams() {
 }
 const ILE_DE_FRANCE_URL = `https://api.airparif.asso.fr/indices/prevision/commune${getIleDeFranceInseeCodeQueryParams()}`;
 
-export async function getAtmoIndicator() {
-  const getCorseAtmo = await fetch(CORSE_URL).then(async (response) => {
-    if (!response.ok) {
-      throw new Error(`getCorseAtmo error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    // TODO: validate data
-    logStep('getCorseAtmo DONE');
-    return data.features as Response[];
+function formatResponse(data: Response[]): AtmoSchema[] {
+  return data.map((feature) => {
+    if (!feature.attributes) throw new Error('attributes is undefined');
+    const { attributes } = feature;
+    const { code_no2, code_so2, code_o3, code_pm10, code_pm25, code_zone } =
+      attributes;
+    return {
+      [code_zone]: {
+        no2: code_no2,
+        so2: code_so2,
+        o3: code_o3,
+        pm10: code_pm10,
+        pm25: code_pm25,
+        max_value: Math.max(code_no2, code_so2, code_o3, code_pm10, code_pm25),
+      },
+    };
   });
-  const getGrandEstAtmo = await fetch(GRAND_EST_URL).then(async (response) => {
-    if (!response.ok) {
-      throw new Error(`getGrandEstAtmo error! status: ${response.status}`);
-    }
+}
 
-    const data = await response.json();
-    // TODO: validate data
-    logStep('getGrandEstAtmo DONE');
-    return data.features as Response[];
-  });
-  const getGuadeloupeAtmo = await fetch(GUADELOUPE_URL).then(
+export async function getAtmoIndicator() {
+  const getCorseAtmo: AtmoSchema[] = await fetch(CORSE_URL).then(
+    async (response) => {
+      if (!response.ok) {
+        throw new Error(`getCorseAtmo error! status: ${response.status}`);
+      }
+      const data = await response.json();
+      logStep('getCorseAtmo DONE');
+      return formatResponse(data.features);
+    },
+  );
+  const getGrandEstAtmo: AtmoSchema[] = await fetch(GRAND_EST_URL).then(
+    async (response) => {
+      type GrandEstResponse = {
+        type: 'Feature';
+        properties: {
+          id: number;
+          date_ech: string; // ISO 8601 date format
+          code_qual: number;
+          lib_qual: string;
+          coul_qual: string; // Hex color code
+          date_dif: string; // ISO 8601 date format
+          source: string;
+          type_zone: string;
+          code_zone: string;
+          lib_zone: string;
+          code_no2: number;
+          code_so2: number;
+          code_o3: number;
+          code_pm10: number;
+          code_pm25: number;
+          x_wgs84: number;
+          y_wgs84: number;
+          x_reg: number;
+          y_reg: number;
+          epsg_reg: string;
+        };
+        geometry: {
+          type: 'Point';
+          coordinates: number[]; // Array of numbers for coordinates
+        };
+      };
+
+      if (!response.ok) {
+        throw new Error(`getGrandEstAtmo error! status: ${response.status}`);
+      }
+
+      const data = await response.json();
+      // TODO: validate data
+      logStep('getGrandEstAtmo DONE');
+      return data.features.map((feature: GrandEstResponse) => {
+        if (!feature.properties) throw new Error('properties is undefined');
+        const { properties } = feature;
+        const { code_no2, code_so2, code_o3, code_pm10, code_pm25, code_zone } =
+          properties;
+        return {
+          [code_zone]: {
+            no2: code_no2,
+            so2: code_so2,
+            o3: code_o3,
+            pm10: code_pm10,
+            pm25: code_pm25,
+            max_value: Math.max(
+              code_no2,
+              code_so2,
+              code_o3,
+              code_pm10,
+              code_pm25,
+            ),
+          },
+        };
+      });
+    },
+  );
+  const getGuadeloupeAtmo: AtmoSchema[] = await fetch(GUADELOUPE_URL).then(
     async (response) => {
       if (!response.ok) {
         throw new Error(`getGuadeloupeAtmo error! status: ${response.status}`);
       }
       const data = await response.json();
-      // TODO: validate data
       logStep('getGuadeloupeAtmo DONE');
-      return data.features as Response[];
+      return formatResponse(data.features);
     },
   );
-  const getHautsDeFranceAtmo = await fetch(HAUTS_DE_FRANCE_URL).then(
-    async (response) => {
-      if (!response.ok) {
-        throw new Error(
-          `getHautsDeFranceAtmo error! status: ${response.status}`,
-        );
-      }
-      const data = await response.json();
-      logStep('getHautsDeFranceAtmo DONE');
-      return data.features as Response[];
-    },
-  );
-  const getMartiniqueAtmo = await fetch(MARTINIQUE_URL).then(
+  const getHautsDeFranceAtmo: AtmoSchema[] = await fetch(
+    HAUTS_DE_FRANCE_URL,
+  ).then(async (response) => {
+    if (!response.ok) {
+      throw new Error(`getHautsDeFranceAtmo error! status: ${response.status}`);
+    }
+    const data = await response.json();
+    logStep('getHautsDeFranceAtmo DONE');
+    return formatResponse(data.features);
+  });
+  const getMartiniqueAtmo: AtmoSchema[] = await fetch(MARTINIQUE_URL).then(
     async (response) => {
       if (!response.ok) {
         throw new Error(`getMartiniqueAtmo error! status: ${response.status}`);
       }
+
+      type MartiniqueResponse = {
+        attributes: {
+          date_ech: number;
+          valeur: number;
+          qualif: string;
+          source: string;
+          type_zone: string;
+          code_zone: string;
+          lib_zone: string;
+          val_no2: number;
+          val_so2: number;
+          val_o3: number;
+          val_pm10: number;
+          couleur: string;
+          x_wgs84: number;
+          y_wgs84: number;
+          x_reg: number;
+          y_reg: number;
+          ESRI_OID: number;
+        };
+      };
       const data = await response.json();
       logStep('getMartiniqueAtmo DONE');
-      return data.features as Response[];
+      return data.features.map((feature: MartiniqueResponse) => {
+        if (!feature.attributes) throw new Error('attributes is undefined');
+        const { attributes } = feature;
+        const { val_no2, val_so2, val_o3, val_pm10, code_zone } = attributes;
+        return {
+          [code_zone]: {
+            no2: val_no2,
+            so2: val_so2,
+            o3: val_o3,
+            pm10: val_pm10,
+            pm25: null,
+            max_value: Math.max(val_no2, val_so2, val_o3, val_pm10),
+          },
+        };
+      });
     },
   );
-  const getNormandieAtmo = await fetch(NORMANDIE_URL).then(async (response) => {
-    if (!response.ok) {
-      throw new Error(`getNormandieAtmo error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    logStep('getNormandieAtmo DONE');
-    return data.features as Response[];
-  });
+  const getNormandieAtmo: AtmoSchema[] = await fetch(NORMANDIE_URL).then(
+    async (response) => {
+      if (!response.ok) {
+        throw new Error(`getNormandieAtmo error! status: ${response.status}`);
+      }
 
+      type NormandieResponse = {
+        type: 'Feature';
+        id: string;
+        bbox: number[];
+        geometry: {
+          type: 'Point';
+          coordinates: number[];
+        };
+        properties: {
+          id: number;
+          date_ech: string;
+          code_qual: number;
+          lib_qual: string;
+          coul_qual: string;
+          date_dif: string;
+          source: string;
+          type_zone: string;
+          code_zone: string;
+          lib_zone: string;
+          code_no2: number;
+          code_so2: number;
+          code_o3: number;
+          code_pm10: number;
+          code_pm25: number;
+          x_wgs84: number;
+          y_wgs84: number;
+          x_reg: number;
+          y_reg: number;
+          epsg_reg: string;
+        };
+      };
+      const data = await response.json();
+      logStep('getNormandieAtmo DONE');
+      return data.features.map((feature: NormandieResponse) => {
+        if (!feature.properties) throw new Error('properties is undefined');
+        const { properties } = feature;
+        const { code_no2, code_so2, code_o3, code_pm10, code_pm25, code_zone } =
+          properties;
+        return {
+          [code_zone]: {
+            no2: code_no2,
+            so2: code_so2,
+            o3: code_o3,
+            pm10: code_pm10,
+            pm25: code_pm25,
+            max_value: Math.max(
+              code_no2,
+              code_so2,
+              code_o3,
+              code_pm10,
+              code_pm25,
+            ),
+          },
+        };
+      });
+    },
+  );
+
+  // TODO: Checker le payload et voir ce qu'on peut faire
   const getNouvelleAquitaineAtmo = await fetch(NOUVELLE_AQUITAINE_URL).then(
     async (response) => {
       if (!response.ok) {
@@ -176,6 +352,7 @@ export async function getAtmoIndicator() {
           `getNouvelleAquitaineAtmo error! status: ${response.status}`,
         );
       }
+
       type AquitaineResponse = {
         type: string;
         id: string;
@@ -193,12 +370,11 @@ export async function getAtmoIndicator() {
           com_long: string | null;
         };
       };
+
       const data = await response.json();
       logStep('getNouvelleAquitaineAtmo DONE');
-      return data.features as AquitaineResponse[];
     },
   );
-
   const getPaysDeLaLoireAtmo = await fetch(PAYS_DE_LA_LOIRE_URL).then(
     async (response) => {
       if (!response.ok) {
@@ -206,19 +382,127 @@ export async function getAtmoIndicator() {
           `getPaysDeLaLoireAtmo error! status: ${response.status}`,
         );
       }
+      type PaysDeLaLoireResponse = {
+        type: 'Feature';
+        id: string;
+        geometry: {
+          type: 'Point';
+          coordinates: number[];
+        };
+        geometry_name: string;
+        properties: {
+          date_ech: string; // ISO 8601 date format
+          code_qual: number;
+          lib_qual: string;
+          coul_qual: string; // Hex color code
+          date_dif: string; // ISO 8601 date format
+          source: string;
+          type_zone: string;
+          code_zone: string;
+          lib_zone: string;
+          code_no2: number;
+          code_so2: number;
+          code_o3: number;
+          code_pm10: number;
+          code_pm25: number;
+          x_wgs84: number;
+          y_wgs84: number;
+          x_reg: number;
+          y_reg: number;
+          epsg_reg: string;
+        };
+      };
       const data = await response.json();
       logStep('getPaysDeLaLoireAtmo DONE');
-      return data.features as Response[];
+
+      return data.features.map((feature: PaysDeLaLoireResponse) => {
+        if (!feature.properties) throw new Error('properties is undefined');
+        const { properties } = feature;
+        const { code_no2, code_so2, code_o3, code_pm10, code_pm25 } =
+          properties;
+        return {
+          no2: code_no2,
+          so2: code_so2,
+          o3: code_o3,
+          pm10: code_pm10,
+          pm25: code_pm25,
+          max_value: Math.max(
+            code_no2,
+            code_so2,
+            code_o3,
+            code_pm10,
+            code_pm25,
+          ),
+        };
+      });
     },
   );
-  const getSudAtmo = await fetch(SUD_URL).then(async (response) => {
-    if (!response.ok) {
-      throw new Error(`getSudAtmo error! status: ${response.status}`);
-    }
-    const data = await response.json();
-    logStep('getSudAtmo DONE');
-    return data.features as Response[];
-  });
+  const getSudAtmo: AtmoSchema[] = await fetch(SUD_URL).then(
+    async (response) => {
+      if (!response.ok) {
+        throw new Error(`getSudAtmo error! status: ${response.status}`);
+      }
+      type SudResponse = {
+        type: 'Feature';
+        id: string;
+        geometry: {
+          type: 'Point';
+          coordinates: number[];
+        };
+        geometry_name: string;
+        properties: {
+          date_ech: string; // ISO 8601 date format
+          code_qual: number;
+          lib_qual: string;
+          coul_qual: string; // Hex color code
+          date_dif: string; // ISO 8601 date format
+          source: string;
+          type_zone: string;
+          code_zone: string;
+          lib_zone: string;
+          code_no2: number;
+          code_so2: number;
+          code_o3: number;
+          code_pm10: number;
+          code_pm25: number;
+          x_wgs84: number;
+          y_wgs84: number;
+          x_reg: number;
+          y_reg: number;
+          epsg_reg: string;
+          conc_no2: number | null;
+          conc_so2: number | null;
+          conc_o3: number | null;
+          conc_pm10: number | null;
+          conc_pm25: number | null;
+        };
+      };
+      const data = await response.json();
+      logStep('getSudAtmo DONE');
+      return data.features.map((feature: SudResponse) => {
+        if (!feature.properties) throw new Error('properties is undefined');
+        const { properties } = feature;
+        const { code_no2, code_so2, code_o3, code_pm10, code_pm25, code_zone } =
+          properties;
+        return {
+          [code_zone]: {
+            no2: code_no2,
+            so2: code_so2,
+            o3: code_o3,
+            pm10: code_pm10,
+            pm25: code_pm25,
+            max_value: Math.max(
+              code_no2,
+              code_so2,
+              code_o3,
+              code_pm10,
+              code_pm25,
+            ),
+          },
+        };
+      });
+    },
+  );
   const getIleDeFranceAtmo = await fetch(ILE_DE_FRANCE_URL, {
     headers: {
       'X-Api-Key': process.env.AIRPARIF_API_KEY as string,
@@ -227,8 +511,98 @@ export async function getAtmoIndicator() {
     if (!response.ok) {
       throw new Error(`getIleDeFranceAtmo error! status: ${response.status}`);
     }
+
+    type IleDeFranceItem = {
+      date: string;
+      no2: string;
+      o3: string;
+      pm10: string;
+      pm25: string;
+      so2: string;
+      indice: string;
+    };
+
+    type IleDeFranceResponse = {
+      [key: string]: IleDeFranceItem[];
+    };
     const data = await response.json();
     logStep('getIleDeFranceAtmo DONE');
-    return data as Response[];
+    return data as Array<IleDeFranceResponse>;
+  });
+  Promise.all([
+    getCorseAtmo,
+    getGrandEstAtmo,
+    getGuadeloupeAtmo,
+    getHautsDeFranceAtmo,
+    getMartiniqueAtmo,
+    getNormandieAtmo,
+    getNouvelleAquitaineAtmo,
+    getPaysDeLaLoireAtmo,
+    getSudAtmo,
+    getIleDeFranceAtmo,
+  ]).then(async (responses) => {
+    type AtmoByDepartment = {
+      [key: string]: {
+        no2: number;
+        so2: number;
+        o3: number;
+        pm10: number;
+        pm25: number;
+        max_value: number;
+      };
+    };
+    let atmoByDepartment = responses.flat();
+    atmoByDepartment = Object.entries(atmoByDepartment).flat();
+
+    // TODO: Il y'a des doublons dans les données
+    // 1. Une des causes est que certaines api retournent des données pour deux jours (ile de france notamment)
+    // 2. Une autre cause est que certaines api retournent des données pour plusieurs communes
+    // A CHECKER !!
+
+    // console.dir(atmoByDepartment, { maxArrayLength: null });
+
+    // const municipalities: Array<MunicipalityJSON> = await new Promise(
+    //   (resolve) => {
+    //     fs.readFile('./data/municipalities.json', 'utf8', async (err, data) => {
+    //       if (err) {
+    //         console.error(err);
+    //         return;
+    //       }
+    //       const municipalities = JSON.parse(data);
+    //       resolve(municipalities);
+    //     });
+    //   },
+    // );
+    // logStep('fetching municipalities DONE');
+    // const atmoRows = [];
+    // for (const municipality of municipalities) {
+    //   const atmoData = atmoByDepartment[municipality.DEP];
+    //   console.log(atmoData);
+    //   // TODO: Le formatage n'est pas ok;
+    //   // if no data for this department, we say that data is not available.
+    //   if (!atmoData) {
+    //     atmoRows.push({
+    //       // diffusion_date: diffusionDate,
+    //       // validity_start: diffusionDate,
+    //       // validity_end: validityEnd,
+    //       municipality_insee_code: municipality.COM,
+    //       data_availability: DataAvailabilityEnum.NOT_AVAILABLE,
+    //     });
+    //     continue;
+    //   }
+    //   atmoRows.push({
+    //     // diffusion_date: diffusionDate,
+    //     // validity_start: diffusionDate,
+    //     // validity_end: validityEnd,
+    //     municipality_insee_code: municipality.COM,
+    //     data_availability: DataAvailabilityEnum.AVAILABLE,
+    //     no2: atmoData.no2,
+    //     so2: atmoData.so2,
+    //     o3: atmoData.o3,
+    //     pm10: atmoData.pm10,
+    //     pm25: atmoData.pm25,
+    //     max_value: atmoData.max_value,
+    //   });
+    // }
   });
 }
