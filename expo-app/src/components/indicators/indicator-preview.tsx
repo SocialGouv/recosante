2bb64eef1f8529d1d1bf181d3af7d67ac710798c
charlesdoiron <doironcharles@gmail.com>
2024-01-09 11:30:18 +0100

    feat(indicator) : Add fake data to generate different indicator in front

--- expo-app/src/components/indicators/indicator-preview.tsx
+++ expo-app/src/components/indicators/indicator-preview.tsx
@@ -7,26 +7,37 @@ import useMunicipality from '~/zustand/municipality/useMunicipality';
 import { cn } from '~/utils/tailwind';
 import { Info } from '~/assets/icons/info';
 import { LineChart } from './graphs/line';
+import { randomIndicatorData } from './mocks/fake';
 
 interface IndicatorPreviewProps {
   indicator: Indicator;
   isFavorite?: boolean;
 }
+
 export function IndicatorPreview(props: IndicatorPreviewProps) {
   const { municipality } = useMunicipality((state) => state);
+  const { status, color, value } = randomIndicatorData();
   return (
     <View
+      style={{
+        borderColor: props.isFavorite ? color : 'transparent',
+      }}
       className={cn(
         '   mx-auto my-5 basis-[47%]  rounded-2xl bg-white p-2 ',
-        `${props.isFavorite ? 'mx-2 -mt-2 border-[3px] border-app-green' : ''}
+        `${props.isFavorite ? 'mx-2 -mt-2 border-[3px] ' : ''}
     }`,
       )}
     >
       <View className="flex flex-row justify-between">
         <View className=" flex w-full justify-center">
-          <View className=" -top-6  mx-auto items-center  rounded-full bg-app-green px-6 py-1">
+          <View
+            className=" -top-6  mx-auto items-center  rounded-full  px-6 py-1"
+            style={{
+              backgroundColor: color,
+            }}
+          >
             <MyText font="MarianneBold" className="uppercase">
-              Moyen
+              {status}
             </MyText>
           </View>
           <View className="-top-6 flex items-end">
@@ -46,7 +57,7 @@ export function IndicatorPreview(props: IndicatorPreviewProps) {
             {municipality?.nom} {municipality?.codesPostaux[0]} -{' '}
             {dayjs().format('DD/MM')}
           </MyText>
-          <LineChart />
+          <LineChart value={value} />
           <MyText className="mt-4 text-xs">
             {IndicatorService.getDescriptionBySlug(props.indicator.slug)}
           </MyText>
