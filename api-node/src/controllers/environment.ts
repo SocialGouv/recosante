72f9a5607b277cd2d63823ef5a422a886d432edb
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-25 18:15:03 +0100

    fix: bump prod api version

--- api-node/src/controllers/environment.ts
+++ api-node/src/controllers/environment.ts
@@ -23,7 +23,7 @@ router.get(
       return;
     }
 
-    const current_app_buildversion_in_production = 9;
+    const current_app_buildversion_in_production = 10;
 
     if (
       parseInt(appbuild as string, 10) > current_app_buildversion_in_production
