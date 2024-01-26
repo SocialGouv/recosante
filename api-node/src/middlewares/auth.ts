a670d60607a0e22b912b57426ffbabb0388a169b
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-26 11:11:18 +0100

    fix: user error 500

--- api-node/src/middlewares/auth.ts
+++ api-node/src/middlewares/auth.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/node';
 import type express from 'express';
 import prisma from '~/prisma';
+import { capture } from '~/third-parties/sentry';
 import type { RequestWithUser } from '~/types/request';
 
 export async function withUser(
@@ -13,14 +14,22 @@ export async function withUser(
     next(new Error('Missing matomo_id'));
     return;
   }
-  const user = await prisma.user.findUnique({
+  let user = await prisma.user.findUnique({
     where: {
       matomo_id,
     },
   });
   if (!user) {
-    next(new Error('User not found'));
-    return;
+    capture('Unexpected user not found so we create a new one', {
+      extra: { matomo_id },
+    });
+    user = await prisma.user.create({
+      data: {
+        matomo_id,
+      },
+    });
+    // next(new Error('User not found'));
+    // return;
   }
   Sentry.setUser(user);
   (req as unknown as RequestWithUser).user = user;
