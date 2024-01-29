8fe632cbd7ec618b38fd468d666613c486c07bbf
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-01-29 11:33:00 +0100

    fix: increment prod version

--- api-node/src/controllers/environment.ts
+++ api-node/src/controllers/environment.ts
@@ -23,7 +23,7 @@ router.get(
       return;
     }
 
-    const current_app_buildversion_in_production = 16;
+    const current_app_buildversion_in_production = 18;
 
     if (
       parseInt(appbuild as string, 10) > current_app_buildversion_in_production
