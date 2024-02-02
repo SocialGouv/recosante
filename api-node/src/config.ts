14c01b5dfb0d5dc9fdd3c3ff088406eb9d52bfa3
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-02-02 10:32:32 +0100

    fix: tipimail from to

--- api-node/src/config.ts
+++ api-node/src/config.ts
@@ -20,8 +20,8 @@ const VERSION = version;
 
 const TIPIMAIL_API_USER = process.env.TIPIMAIL_API_USER;
 const TIPIMAIL_API_KEY = process.env.TIPIMAIL_API_KEY;
-const TIPIMAIL_EMAIL_TO = process.env.TIPIMAIL_EMAIL_TO;
-const TIPIMAIL_EMAIL_FROM = process.env.TIPIMAIL_EMAIL_FROM;
+const TIPIMAIL_EMAIL_TO = 'contact@recosante.beta.gouv.fr';
+const TIPIMAIL_EMAIL_FROM = 'contact@recosante.fabrique.social.gouv.fr';
 
 const MATOMO_URL = process.env.MATOMO_URL;
 const MATOMO_IDSITE_1 = '107';
