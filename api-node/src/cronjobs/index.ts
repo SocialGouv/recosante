32e3d289925e962ec1c7b50752778a92d21a8c80
Arnaud AMBROSELLI <arnaud.ambroselli@yahoo.fr>
2024-02-08 11:13:59 +0100

    fix: sentry in cronjob

--- api-node/src/cronjobs/index.ts
+++ api-node/src/cronjobs/index.ts
@@ -1,10 +1,31 @@
 import 'dotenv/config';
 import '~/prisma';
+import * as Sentry from '@sentry/node';
 
 import { initMunicipalities } from './municipalities';
 import { initRecommandations } from './recommandations';
 import { initAggregators } from './aggregators';
 import { initNotifications } from './notifications';
+import { ENVIRONMENT, SENTRY_KEY, VERSION } from '~/config';
+
+const sentryEnabled = process.env.NODE_ENV !== 'development';
+
+if (sentryEnabled) {
+  Sentry.init({
+    dsn: SENTRY_KEY,
+    environment: `cronjobs-${ENVIRONMENT}`,
+    release: VERSION,
+    integrations: [
+      // Automatically instrument Node.js libraries and frameworks
+      ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations(),
+    ],
+
+    // Set tracesSampleRate to 1.0 to capture 100%
+    // of transactions for performance monitoring.
+    // We recommend adjusting this value in production
+    tracesSampleRate: 0.05,
+  });
+}
 
 Promise.resolve()
   .then(initMunicipalities) //
--- api-node/src/utils/recommandations.ts
+++ api-node/src/utils/recommandations.ts
@@ -1,6 +1,7 @@
 import type { SeasonEnum, IndicatorsSlugEnum } from '@prisma/client';
 import { getCellContentFromGoogleSheet } from '~/service/google-sheet';
 import prisma from '~/prisma';
+import { capture } from '~/third-parties/sentry';
 
 type RangeFromGoogleSheet = {
   range: string;
@@ -42,63 +43,80 @@ export async function getRecommandationsFromGoogleSheet() {
   })) as unknown as RangeFromGoogleSheet;
   // for each row, create or update recommandation
   for (const [rowIndex, value] of Object.entries(recosSheet.values)) {
-    const indicator = value[indicatorSlugIndex] as IndicatorsSlugEnum;
-    if (!indicator) continue;
-    const indicator_values = value[indicatorValuesIndex]
-      ? value[indicatorValuesIndex]
-          .split(',')
-          .map((indicator_value) => indicator_value.trim())
-      : [];
-    const type_weather_alert = value[typeWeatherAlertIndex];
-    const seasons = value[seasonsIndex]
-      .split(',')
-      .map((season) => season.trim()) as Array<SeasonEnum>;
-    const recommandation_contents = [];
-    for (const recommandationContentIndex of recommandationContentsIndex) {
-      if (!recommandationContentIndex) continue;
-      recommandation_contents.push(value[recommandationContentIndex].trim());
-    }
-
-    for (const indicator_value of indicator_values) {
-      if (indicator_value === '') continue;
-      for (const [
-        recommandationIndex,
-        recommandation_content,
-      ] of Object.entries(recommandation_contents)) {
-        if (!recommandation_content || recommandation_content === '-') {
-          continue;
+    try {
+      const indicator = value[indicatorSlugIndex] as IndicatorsSlugEnum;
+      if (!indicator) continue;
+      const indicator_values = value[indicatorValuesIndex]
+        ? value[indicatorValuesIndex]
+            .split(',')
+            .map((indicator_value) => indicator_value.trim())
+        : [];
+      const type_weather_alert = value[typeWeatherAlertIndex];
+      const seasons = value[seasonsIndex]
+        .split(',')
+        .map((season) => season.trim()) as Array<SeasonEnum>;
+      const recommandation_contents = [];
+      for (const recommandationContentIndex of recommandationContentsIndex) {
+        try {
+          if (!recommandationContentIndex) continue;
+          recommandation_contents.push(
+            value[recommandationContentIndex]?.trim(),
+          );
+        } catch (error: any) {
+          capture(error, {
+            extra: {
+              rowIndex,
+              value,
+              recommandationContentIndex,
+              recommandationContentsIndex,
+            },
+          });
         }
-
-        const recommandation_id = [
-          rowIndex.padStart(4, '0'),
-          recommandationIndex,
-        ].join('-');
-        const unique_key = [
-          recommandation_id,
-          indicator,
-          indicator_values.join('-'),
-          indicator_value,
-          type_weather_alert,
-          seasons.join('-'),
-        ].join('_');
-        await prisma.recommandation.upsert({
-          where: {
-            unique_key,
-          },
-          create: {
-            unique_key,
-            recommandation_id,
-            recommandation_content,
-            indicator,
-            indicator_value: parseInt(indicator_value, 10),
-            type_weather_alert,
-            seasons,
-          },
-          update: {
-            recommandation_content,
-          },
-        });
       }
+
+      for (const indicator_value of indicator_values) {
+        if (indicator_value === '') continue;
+        for (const [
+          recommandationIndex,
+          recommandation_content,
+        ] of Object.entries(recommandation_contents)) {
+          if (!recommandation_content || recommandation_content === '-') {
+            continue;
+          }
+
+          const recommandation_id = [
+            rowIndex.padStart(4, '0'),
+            recommandationIndex,
+          ].join('-');
+          const unique_key = [
+            recommandation_id,
+            indicator,
+            indicator_values.join('-'),
+            indicator_value,
+            type_weather_alert,
+            seasons.join('-'),
+          ].join('_');
+          await prisma.recommandation.upsert({
+            where: {
+              unique_key,
+            },
+            create: {
+              unique_key,
+              recommandation_id,
+              recommandation_content,
+              indicator,
+              indicator_value: parseInt(indicator_value, 10),
+              type_weather_alert,
+              seasons,
+            },
+            update: {
+              recommandation_content,
+            },
+          });
+        }
+      }
+    } catch (error: any) {
+      capture(error, { extra: { rowIndex, value } });
     }
   }
   console.log('Recommandations upserted');
