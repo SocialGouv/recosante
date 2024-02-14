c9297432cfe4f819839b17d1ed3f077ee33e62ad
charlesdoiron <doironcharles@gmail.com>
2024-02-14 17:31:21 +0100

    fix: Refactor NavLinks component to handle empty label

--- frontend-next/src/components/NavLinks.tsx
+++ frontend-next/src/components/NavLinks.tsx
@@ -9,7 +9,6 @@ export function NavLinks() {
   let timeoutRef = useRef<number | null>(null);
 
   return [
-    ['Accessibilité : partiellement conforme', '/accessibilite'],
     ['Mentions légales', '/mentions-legales/'],
     ['Données personnelles', '/donnees-personnelles/'],
     ['Partenaires', '/partenaires/'],
@@ -17,39 +16,46 @@ export function NavLinks() {
     ['Gestion des cookies', '/cookies/'],
     ['Statistiques', '/stats/'],
     ['Contactez-nous', 'mailto:contact@recosante.beta.gouv.fr'],
-  ].map(([label, href], index) => (
-    <Link
-      key={label}
-      href={href}
-      target='_blank'
-      className='relative text-nowrap -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-0'
-      onMouseEnter={() => {
-        if (timeoutRef.current) {
-          window.clearTimeout(timeoutRef.current);
-        }
-        setHoveredIndex(index);
-      }}
-      onMouseLeave={() => {
-        timeoutRef.current = window.setTimeout(() => {
-          setHoveredIndex(null);
-        }, 200);
-      }}
-    >
-      <AnimatePresence>
-        {hoveredIndex === index && (
-          <motion.span
-            className='absolute inset-0 rounded-lg bg-gray-100'
-            layoutId='hoverBackground'
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1, transition: { duration: 0.15 } }}
-            exit={{
-              opacity: 0,
-              transition: { duration: 0.15 },
-            }}
-          />
-        )}
-      </AnimatePresence>
-      <span className='relative z-10 text-xs'>{label}</span>
-    </Link>
-  ));
+    ['', ''],
+    ['Accessibilité : partiellement conforme', '/accessibilite'],
+  ].map(([label, href], index) => {
+    if (!label) {
+      return <span key={index} className='text-xs' />;
+    }
+    return (
+      <Link
+        key={label}
+        href={href}
+        target='_blank'
+        className=' relative text-nowrap -mx-3 -my-2 rounded-lg px-3 py-2 text-sm text-gray-700 transition-colors delay-150 hover:text-gray-900 hover:delay-0'
+        onMouseEnter={() => {
+          if (timeoutRef.current) {
+            window.clearTimeout(timeoutRef.current);
+          }
+          setHoveredIndex(index);
+        }}
+        onMouseLeave={() => {
+          timeoutRef.current = window.setTimeout(() => {
+            setHoveredIndex(null);
+          }, 200);
+        }}
+      >
+        <AnimatePresence>
+          {hoveredIndex === index && (
+            <motion.span
+              className='absolute inset-0 rounded-lg bg-gray-100'
+              layoutId='hoverBackground'
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1, transition: { duration: 0.15 } }}
+              exit={{
+                opacity: 0,
+                transition: { duration: 0.15 },
+              }}
+            />
+          )}
+        </AnimatePresence>
+        <span className='relative z-10 text-xs'>{label}</span>
+      </Link>
+    );
+  });
 }
