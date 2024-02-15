d207bbe97126700d5606c8de1be94915cb2aeb8b
Adrien Chauve <adrien@chauve.eu>
2024-02-15 22:44:11 +0100

    ifix: try force gatsby to reload home page to go back to next

--- frontend/src/components/layout/header/Logos.js
+++ frontend/src/components/layout/header/Logos.js
@@ -2,17 +2,16 @@ import React from "react";
 
 import Marianne from "components/base/Marianne";
 import Logo from "components/misc/Logo";
-import { Link } from "gatsby";
 
 export default function Logos({ className }) {
   return (
-    <Link
-      to="/"
+    <a
+      href="/"
       className={className}
       aria-label="Aller à l’accueil - Recosanté - République Française, Liberté Égalité Fraternité"
     >
       <Marianne />
       <Logo link />
-    </Link>
+    </a>
   );
 }
