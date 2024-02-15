c865e010aa5d27606781782acd73bf70d81a81f7
charlesdoiron <doironcharles@gmail.com>
2024-02-15 17:01:04 +0100

    feat: Refactor getMunicipalitiesParams to include multiple indicators

--- frontend-next/src/services/page-builder.ts
+++ frontend-next/src/services/page-builder.ts
@@ -2,17 +2,31 @@ import { Municipality } from '@/types/municipality';
 import municipalitiesJson from '../data/municipalities.json';
 import { slugify } from '@/utils/slugify';
 
+const indicators = [
+  'indice-atmo',
+  'indice-uv',
+  'pollens',
+  'alerte-meteo',
+  'eau-de-baignade',
+];
+
 const municipalities = municipalitiesJson as Municipality[];
 export namespace PageBuilderService {
   export function getMunicipalitiesParams() {
-    return municipalities.map((municipality) => {
-      return {
-        params: {
-          'insee-code': municipality.COM,
-          city: slugify(municipality.NCCENR),
-          indicator: 'population',
-        },
-      };
-    });
+    return municipalities
+      .map((municipality) => {
+        return indicators
+          .map((indicator) => {
+            return {
+              params: {
+                'insee-code': municipality.COM,
+                city: slugify(municipality.NCCENR),
+                indicator: indicator,
+              },
+            };
+          })
+          .flat();
+      })
+      .flat();
   }
 }
