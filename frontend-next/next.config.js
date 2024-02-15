cc8db51a278c45a3fb44f83a9a135deaa1835ce8
Adrien Chauve <adrien@chauve.eu>
2024-02-15 22:37:13 +0100

    fix: use catch all redirects in next config

--- frontend-next/next.config.js
+++ frontend-next/next.config.js
@@ -7,61 +7,18 @@ const PATH = process.env.NEXT_PUBLIC_GATSBY_INTERN_URL || 'http://frontend';
 const nextConfig = {
   output: 'standalone',
   trailingSlash: true,
+
   rewrites: async () => {
     return {
-      beforeFiles: [
-        {
-          source: '/place/:slug*/',
-          destination: `${PATH}/place/:slug*/`,
-        },
-        {
-          source: '/articles/:slug*/',
-          destination: `${PATH}/articles/:slug*/`,
-        },
-        {
-          source: '/static/:slug*',
-          destination: `${PATH}/static/:slug*`,
-        },
-        {
-          source: '/',
-          has: [
-            {
-              type: 'query',
-              key: 'iframe',
-              value: '1',
-            },
-          ],
-          destination: `${PATH}/`,
-        },
-      ],
       afterFiles: [
+        // catch-all to gatsby (js scripts, json data, etc.), with and without trailing slash
         {
-          source: '/accessibilite',
-          destination: `${PATH}/accessibilite/`,
+          source: '/:slug*/',
+          destination: `${PATH}/:slug*/`,
         },
         {
-          source: '/mentions-legales',
-          destination: `${PATH}/mentions-legales/`,
-        },
-        {
-          source: '/donnees-personnelles',
-          destination: `${PATH}/donnees-personnelles/`,
-        },
-        {
-          source: '/cookies',
-          destination: `${PATH}/cookies/`,
-        },
-        {
-          source: '/partenaires',
-          destination: `${PATH}/partenaires/`,
-        },
-        {
-          source: '/recommandations',
-          destination: `${PATH}/recommandations/`,
-        },
-        {
-          source: '/stats',
-          destination: `${PATH}/stats/`,
+          source: '/:slug*',
+          destination: `${PATH}/:slug*`,
         },
       ],
     };
