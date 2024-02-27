203113c59644e67debc6bbc7295e1485d32cd207
charlesdoiron <doironcharles@gmail.com>
2024-02-27 10:13:21 +0100

    feat(iframe): add iframe page

--- frontend-next/src/components/download-page/hero-banner.tsx
+++ frontend-next/src/components/download-page/hero-banner.tsx
@@ -77,35 +77,32 @@ function BackgroundIllustration(props: React.ComponentPropsWithoutRef<'div'>) {
 
 export function Hero() {
   return (
-    <div className='overflow-hidden py-8 sm:py-8 lg:pb-32 xl:pb-36 '>
+    <div className='overflow-hidden pt-2 lg:pb-32 xl:pb-36 '>
       <Container>
-        <div className='md:grid lg:grid-cols-12 lg:gap-x-8 grid-cols-2 lg:gap-y-12'>
-          <div className='relative z-10 mx-auto  lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6'>
-            <h1 className='lg:text-5xl text-4xl  font-bold  tracking-tight text-app-primary  font-app uppercase'>
-              Votre nouvelle application de santé au quotidien.
-            </h1>
-            <p className='mt-6 text-lg text-gray-600 '>
-              Une application gratuite et personnalisable pour protéger votre
-              santé et celle de votre famille.
+        <div>
+          <div className='relative z-50  mx-auto  lg:col-span-7 lg:max-w-none lg:pt-6 xl:col-span-6'>
+            <div className='flex items-start flex-col '>
+              <GouvLogo className='-ml-2 md:max-w-[120px] max-w-[70px] ' />
+              <h1 className='md:text-3xl text-xl  font-bold  tracking-tight  text-app-primary  font-app uppercase '>
+                RECOSANTé,
+                <br /> VOTRE NOUVELLE APPLICATION POUR LA SANTÉ AU QUOTIDIEN.
+              </h1>
+            </div>
+
+            <p className=' text-md md:text-xl mt-4  text-["#2D2D2D"]  '>
+              Découvrez comment l’environnement impacte votre santé.
+              <br /> Protégez vous de la pollution de l’air, de l’eau, des UV ou
+              du pollen.
             </p>
-            <div className='mt-12 flex md:justify-start space-x-2 justify-center '>
+            <div className=' z-50 scale-[0.7] md:scale-100 flex items-center space-x-4  md:mt-6 mt-2 mx-auto justify-center '>
               <AppStoreLink />
               <GoogleStoreLink />
             </div>
-            <p className='text-center text-sm mt-8 text-gray-500 lg:text-left'>
-              Les recommandations sont issues de sources fiables et validés par
-              le Haut conseil de santé publique et Santé publique France.
-            </p>
-
-            <div className='relative mt-24 space-x-4  flex justify-start items-center'>
-              <GouvLogo className='h-32 w-auto -ml-4' />
-              <Logo className='h-20 w-auto' />
-            </div>
           </div>
-          <div className='relative hidden md:block  mt-10 sm:mt-20 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6'>
-            <BackgroundIllustration className='absolute left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0' />
-            <div className='-mx-4  px-9 [mask-image:linear-gradient(to_bottom,white_20%,transparent)] sm:mx-0  lg:-inset-x-10 lg:-bottom-20 lg:-top-4 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32'>
-              <PhoneFrame className='mx-auto max-w-[466px]' priority>
+          <div className='relative z-10 left-0 right-0 mt-8 lg:col-span-5 lg:row-span-2 lg:mt-0 xl:col-span-6'>
+            <BackgroundIllustration className='absolute  left-1/2 top-4 h-[1026px] w-[1026px] -translate-x-1/3 stroke-gray-300/70 [mask-image:linear-gradient(to_bottom,white_20%,transparent_75%)] sm:top-16 sm:-translate-x-1/2 lg:-top-16 lg:ml-12 xl:-top-14 xl:ml-0' />
+            <div className='-mx-4  px-9  [mask-image:linear-gradient(to_bottom,white_20%,transparent)] sm:mx-0  lg:-inset-x-10 lg:-bottom-20 lg:-top-4 lg:h-auto lg:px-0 lg:pt-10 xl:-bottom-32'>
+              <PhoneFrame className='mx-auto  max-w-[266px] ' priority>
                 <LandingMobileScreen />
               </PhoneFrame>
             </div>
