40e219eb4efca0211e3b17b3de135ab48114b42f
charlesdoiron <doironcharles@gmail.com>
2024-02-27 17:08:42 +0100

    hotfix: Update iframe style to include min-height

--- frontend/iframe/index.js
+++ frontend/iframe/index.js
@@ -12,7 +12,7 @@ const iframe = document.createElement("iframe");
 const iframeAttributes = {
   src,
   id: "recosante-iframe",
-  style: "border: none; width: 100%; display: block; margin: 0 auto;",
+  style: "border: none; width: 100%; display: block; margin: 0 auto; min-height: 510px",
   allowfullscreen: true,
   webkitallowfullscreen: true,
   mozallowfullscreen: true,
