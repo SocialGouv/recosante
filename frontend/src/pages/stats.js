c2cb996d5fdbaea33a4e78d9485482af5cdfe5c3
charlesdoiron <doironcharles@gmail.com>
2024-03-05 09:35:34 +0100

    Update stats page with new iframe

--- frontend/src/pages/stats.js
+++ frontend/src/pages/stats.js
@@ -1,12 +1,12 @@
 import React from "react";
-
 import Web from "components/layout/Web";
-import Statistiques from "components/Statistiques";
 
 export default function stats(props) {
   return (
     <Web title={"Statistiques"}>
-      <Statistiques />
+      <div className="h-screen">
+      <iframe src="https://metabase-recosante.fabrique.social.gouv.fr/public/dashboard/3d03787d-0854-4d81-8892-5f07d8dff835" frameborder="0" width="100%" height="100%" allowtransparency ></iframe>
+      </div>
     </Web>
   );
 }
