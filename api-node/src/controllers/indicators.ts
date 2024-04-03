4837c278a32b333753fd0e67e43161c576f40444
charlesdoiron <doironcharles@gmail.com>
2024-04-03 14:00:31 +0200

    hotfix: remove dringking water

--- api-node/src/controllers/indicators.ts
+++ api-node/src/controllers/indicators.ts
@@ -13,7 +13,7 @@ import { indicatorsList } from '~/getters/indicators_list';
 import { withUser } from '~/middlewares/auth';
 import utc from 'dayjs/plugin/utc';
 import { getBathingWaterFromMunicipalityAndDate } from '~/getters/bathing_water';
-import { getDrinkingWaterFromUdi } from '~/getters/drinking_water';
+// import { getDrinkingWaterFromUdi } from '~/getters/drinking_water';
 dayjs.extend(utc);
 
 const router = express.Router();
@@ -99,16 +99,16 @@ router.get(
 
       if (bathingWater) indicators.push(bathingWater);
 
-      const drinkingWater = await getDrinkingWaterFromUdi({
-        udi: req.user.udi,
-        municipality_insee_code,
-        date_UTC_ISO: dayjs().utc().toISOString(),
-      });
-      if (drinkingWater instanceof Error) {
-        next(drinkingWater);
-        return;
-      }
-      if (drinkingWater) indicators.push(drinkingWater);
+      // const drinkingWater = await getDrinkingWaterFromUdi({
+      //   udi: req.user.udi,
+      //   municipality_insee_code,
+      //   date_UTC_ISO: dayjs().utc().toISOString(),
+      // });
+      // if (drinkingWater instanceof Error) {
+      //   next(drinkingWater);
+      //   return;
+      // }
+      // if (drinkingWater) indicators.push(drinkingWater);
 
       res.status(200).send({ ok: true, data: indicators });
     },
