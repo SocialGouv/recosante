eb9b59ba8586c4e9f62a45441df66b7b1df1fe23
Arnaud Ambroselli <31724752+arnaudambro@users.noreply.github.com>
2024-04-08 20:33:59 +0200

    fix: in app rating asked from backend (#516)

---
+++ api-node/prisma/migrations/20240408183137_notification_index_by_user_id/migration.sql
@@ -0,0 +1,2 @@
+-- CreateIndex
+CREATE INDEX "notification_user_id" ON "Notification"("user_id");
--- api-node/src/utils/user.ts
+++ api-node/src/utils/user.ts
@@ -1,21 +1,30 @@
 import dayjs from 'dayjs';
 import type { User } from '@prisma/client';
+import prisma from '~/prisma';
 
-export function canAskReviewForUser(user: User | null) {
+export async function canAskReviewForUser(user: User | null) {
   if (!user) return false; // no user
   if (Number(user.appbuild) < 24) {
     console.log('store review unavailable before');
-    return false; // store review unavailable before
-  }
-  if (!user?.notifications_sent) {
-    console.log('not enough experience with the app');
-    return false; // not enough experience with the app
+    return false;
   }
   if (user?.asked_for_review) {
-    console.log('already done');
-    return false; // already done
+    console.log('already done manually');
+    return false;
   }
-  if (!user?.asked_for_review_latest_at) return true; // no date
+  const notificationsSent = await prisma.notification.count({
+    where: {
+      user_id: user.id,
+    },
+  });
+  if (!notificationsSent) {
+    console.log('not enough experience with the app');
+    return false;
+  }
+  if (!user?.asked_for_review_latest_at) {
+    console.log('no date, meaning never asked before so we can ask');
+    return true;
+  }
   if (dayjs().diff(dayjs(user?.asked_for_review_latest_at), 'months') < 3) {
     console.log('too recent');
     return false; // too recent
