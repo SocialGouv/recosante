bf5021a43c35441796d30876787061827e7fe5ad
leo.sajous.ext <leo.sajous.ext@geodis.com>
2025-04-07 17:56:01 +0200

    chore(scripts): uncomment uwsgi and celery commands in start scripts

--- api/start_all.sh
+++ api/start_all.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 .venv/bin/flask db upgrade
-#.venv/bin/uwsgi --http 0.0.0.0:8080 --wsgi wsgi:app --enable-threads --master --processes 10 --http-timeout 1800 --attach-daemon './start_celery.sh' --attach-daemon './start_beat.sh'
+.venv/bin/uwsgi --http 0.0.0.0:8080 --wsgi wsgi:app --enable-threads --master --processes 10 --http-timeout 1800 --attach-daemon './start_celery.sh' --attach-daemon './start_beat.sh'
--- api/start_beat.sh
+++ api/start_beat.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-#.venv/bin/celery -A ecosante.celery_worker.celery beat --loglevel=INFO
+.venv/bin/celery -A ecosante.celery_worker.celery beat --loglevel=INFO
