f32e4e58a7d5cd3afd193745c92bf6a87e8b5b38
leo.sajous.ext <leo.sajous.ext@geodis.com>
2025-04-07 15:12:31 +0200

    refactor(aggregators): trigger cron jobs from nod api and disable old api tasks

--- api-node/src/cronjobs/index.ts
+++ api-node/src/cronjobs/index.ts
@@ -32,7 +32,7 @@ const isLocalDevelopment = process.env.NODE_ENV === 'development';
 //const isReviewBranch = ENVIRONMENT === 'development';
 
 //TODO: rollback isReviewBranch
-if (isLocalDevelopment) {
+if (!isLocalDevelopment) {
   Promise.resolve()
     .then(initIndicatorsCleaning) //
     .then(initMunicipalities) //
