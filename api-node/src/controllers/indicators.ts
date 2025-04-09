039364fe71f2f62b092b150f5ddfeaf0aedd8bf4
leo.sajous.ext <leo.sajous.ext@geodis.com>
2025-04-09 14:57:51 +0200

    chore(scripts): drinking water data fetch

--- api-node/src/controllers/indicators.ts
+++ api-node/src/controllers/indicators.ts
@@ -12,8 +12,7 @@ import { indicatorsList } from '~/getters/indicators_list';
 import { withUser } from '~/middlewares/auth';
 import utc from 'dayjs/plugin/utc';
 import { getBathingWaterFromMunicipalityAndDate } from '~/getters/bathing_water';
-// import { getDrinkingWaterFromUdi } from '~/getters/drinking_water';
-import { IndicatorsSlugEnum } from '@prisma/client';
+import { getDrinkingWaterFromUdi } from '~/getters/drinking_water';
 dayjs.extend(utc);
 
 const router = express.Router();
@@ -107,18 +106,18 @@ router.get(
 
       if (bathingWater) indicators.push(bathingWater);
 
-      // if (Number(req.user.appbuild) > 62) {
-      //   const drinkingWater = await getDrinkingWaterFromUdi({
-      //     udi: req.user.udi,
-      //     municipality_insee_code,
-      //     date_UTC_ISO: dayjs().utc().toISOString(),
-      //   });
-      //   if (drinkingWater instanceof Error) {
-      //     next(drinkingWater);
-      //     return;
-      //   }
-      //   if (drinkingWater) indicators.push(drinkingWater);
-      // }
+      if (Number(req.user.appbuild) > 62) {
+        const drinkingWater = await getDrinkingWaterFromUdi({
+          udi: req.user.udi,
+          municipality_insee_code,
+          date_UTC_ISO: dayjs().utc().toISOString(),
+        });
+        if (drinkingWater instanceof Error) {
+          next(drinkingWater);
+          return;
+        }
+        if (drinkingWater) indicators.push(drinkingWater);
+      }
 
       res.status(200).send({ ok: true, data: indicators });
     },
